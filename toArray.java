107a919a296883df4f12a4551b72a740d32e12a0
saifullah <saifullahahad0@gmail.com>
2026-09-03 23:10:08 +0530

    convert string to char

---
+++ toArray.java
@@ -0,0 +1,16 @@
+import java.io.StringReader;
+import java.util.*;
+
+public class toArray {
+
+    public static void main(String[] args) {
+
+      String str = "saifullah";
+      char[] arr = str.toCharArray();
+
+
+       for(char ch : arr){
+        System.out.println(ch);
+       }
+    }
+}
