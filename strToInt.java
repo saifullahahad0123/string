b5e038fffcb6903715e5f7b15425fb4ab0fd22aa
saifullah <saifullahahad0@gmail.com>
2026-09-03 23:01:03 +0530

    convert string to integer

---
+++ strToInt.java
@@ -0,0 +1,14 @@
+import java.io.StringReader;
+import java.util.*;
+
+public class strToInt {
+
+    public static void main(String[] args) {
+
+      String str = "1234567";
+      int n = Integer.parseInt(str);
+
+
+        System.out.println(n+1);
+    }
+}
